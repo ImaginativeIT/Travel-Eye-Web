cb205c950f99b7efcac76c89bc0577cffc33d682
Lakshitha-Gayashan <lakshitha.imaginative@outlook.com>
2020-03-21 19:50:55 +0530

    created basic components

---
+++ src/Components/place-selection/place-selection.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+class PlaceSelection extends Component {
+  render() {
+    return <h1>Place selection page</h1>;
+  }
+}
+
+export default PlaceSelection;
